cc052d7adfd3b2e0b8c13d31ce40f9804d2b3585
Manali Shah <manali@zealousweb.com>
2018-10-03 11:19:20 +0530

    Added Location File

---
+++ ChooseLocationPopUp.h
@@ -0,0 +1,15 @@
+//
+//  ChooseLocationPopUp.h
+//  TableTalk
+//
+//  Created by Yogesh Rathod on 22/09/17.
+//  Copyright © 2017 Zealousweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ChooseLocationPopUp : UIViewController
+
+
+@end
